d39db708bc9198d9e0046d991174941e8a0e64bf
PavithraMadhushini <65077990+PavithraMadhushini@users.noreply.github.com>
2021-03-17 13:30:38 +0530

    deploy my react app to github

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Content from "./content";
+import Footer from "./footer";
+import Loading from "./loading";
+import NavBar from "./nav-bar";
+import Highlight from "./highlight";
+
+export { Content, Footer, Loading, NavBar, Highlight };
---
+++ src/views/home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import { Hero, Content } from "../components";
+
+const Home = () => (
+  <Fragment>
+    
+    <hr />
+    <Content />
+  </Fragment>
+);
+
+export default Home;
